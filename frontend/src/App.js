79708f25213458f7f7a4ced9484215d7a8867541
Luis fernando <fernandocpdoba@gmail.com>
2019-10-11 16:43:38 -0300

    Criado tela de login

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+   <div className="container">
+     <img src={logo} alt="AirCnC"/>
+     <div className="content">
+       <p>
+         Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+       </p>
+
+       <form>
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            id="email" 
+            type="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button type="submit" className="btn">Entrar</button>
+       </form>
+     </div>
+   </div>
+  );
+}
+
+export default App;
